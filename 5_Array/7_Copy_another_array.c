418754daf330191caeaac7c66fab6fd19c222825
Palak Thakkar <thakkarp559@gmail.com>
2026-09-23 22:59:58 +0530

    done

---
+++ 5_Array/7_Copy_another_array.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int i, arr1[100],arr2[100];
+    int size;
+
+    printf("Enter array size: ");
+    scanf("%d", &size);
+
+   printf("Enter array elements: ");
+    for(i=0;i<size;i++)
+     {
+        scanf("%d",&size);
+     }
+
+
+
+
+}
